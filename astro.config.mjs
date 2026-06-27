b59ffd96aa2ed48de65135ebf85d59412f91daa0
hagi-masa <hagi.masa.hagi@gmail.com>
2026-06-27 22:26:12 +0900

    URL構造を修正＆サイトマップ追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -6,4 +7,5 @@ export default defineConfig({
   site: 'https://yurieshiraishi.com',
   // base: '/clarinet-lessons/',
   compressHTML: true,
+  integrations: [sitemap()],
 });
