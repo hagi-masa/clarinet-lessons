e041f2ba1d4fc272e055de8fadf59084782c120e
hagi-masa <hagi.masa.hagi@gmail.com>
2026-06-27 19:15:41 +0900

    独自ドメイン設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  // TODO: 公開時に実際のドメインに変更してください
-  site: 'https://hagi-masa.github.io',
-  // site: 'https://yurieshiraishi.com',
-  base: '/clarinet-lessons/',
+  // 公開時のドメインを設定
+  site: 'https://yurieshiraishi.com',
+  // base: '/clarinet-lessons/',
   compressHTML: true,
 });
