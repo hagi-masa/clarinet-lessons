4182fba94e1288aa29f85b7ef55a43f0321d664d
hagi-masa <hagi.masa.hagi@gmail.com>
2026-06-27 18:09:40 +0900

    修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // TODO: 公開時に実際のドメインに変更してください
-  site: 'https://yurieshiraishi.com',
+  site: 'https://hagi-masa.github.io',
+  // site: 'https://yurieshiraishi.com',
+  base: '/clarinet-lessons/',
   compressHTML: true,
 });
